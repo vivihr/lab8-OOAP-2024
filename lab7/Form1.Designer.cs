f276aee632b53d5e705e92dbbb7f61e100035c67
admin <admin@DESKTOP-M40D7GV>
2024-05-24 15:08:26 +0300

    Add project files.

---
+++ lab7/Form1.Designer.cs
@@ -0,0 +1,278 @@
+﻿
+namespace lab7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.Button btnSend;
+            System.Windows.Forms.Button btnSent2;
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.lstChatHistory = new System.Windows.Forms.ListBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtMessage2 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            btnSend = new System.Windows.Forms.Button();
+            btnSent2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnSend
+            // 
+            btnSend.Location = new System.Drawing.Point(122, 605);
+            btnSend.Name = "btnSend";
+            btnSend.Size = new System.Drawing.Size(103, 36);
+            btnSend.TabIndex = 22;
+            btnSend.Text = "Sent";
+            btnSend.UseVisualStyleBackColor = true;
+            btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // btnSent2
+            // 
+            btnSent2.Location = new System.Drawing.Point(674, 596);
+            btnSent2.Name = "btnSent2";
+            btnSent2.Size = new System.Drawing.Size(103, 36);
+            btnSent2.TabIndex = 29;
+            btnSent2.Text = "Sent";
+            btnSent2.UseVisualStyleBackColor = true;
+            btnSent2.Click += new System.EventHandler(this.btnSent2_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(40, 328);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(115, 152);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 16;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 56);
+            this.label2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(211, 30);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "hey!! what`s up?";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(529, 94);
+            this.label3.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(345, 30);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "hello love! i`m in the store.";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(529, 135);
+            this.label4.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(222, 30);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "do u need smth?";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(14, 170);
+            this.label5.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(399, 30);
+            this.label5.TabIndex = 20;
+            this.label5.Text = "omg yes i want some patatas, ";
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(28, 544);
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(377, 36);
+            this.txtMessage.TabIndex = 21;
+            // 
+            // lstChatHistory
+            // 
+            this.lstChatHistory.FormattingEnabled = true;
+            this.lstChatHistory.ItemHeight = 30;
+            this.lstChatHistory.Location = new System.Drawing.Point(228, 246);
+            this.lstChatHistory.Name = "lstChatHistory";
+            this.lstChatHistory.Size = new System.Drawing.Size(423, 244);
+            this.lstChatHistory.TabIndex = 24;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(14, 200);
+            this.label6.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(368, 30);
+            this.label6.TabIndex = 25;
+            this.label6.Text = "croquetas, paella and sangria";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("RomanD", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(22, 506);
+            this.label7.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(107, 35);
+            this.label7.TabIndex = 26;
+            this.label7.Text = "user 1";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("RomanD", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label8.Location = new System.Drawing.Point(767, 506);
+            this.label8.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(107, 35);
+            this.label8.TabIndex = 27;
+            this.label8.Text = "user 2";
+            // 
+            // txtMessage2
+            // 
+            this.txtMessage2.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.txtMessage2.Location = new System.Drawing.Point(497, 544);
+            this.txtMessage2.Name = "txtMessage2";
+            this.txtMessage2.Size = new System.Drawing.Size(377, 36);
+            this.txtMessage2.TabIndex = 28;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(723, 332);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(126, 148);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 30;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("RomanD", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(761, 51);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 35);
+            this.label1.TabIndex = 31;
+            this.label1.Text = "user 2";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("RomanD", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label9.Location = new System.Drawing.Point(14, 9);
+            this.label9.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(113, 35);
+            this.label9.TabIndex = 32;
+            this.label9.Text = "user 1";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("RomanD", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label10.Location = new System.Drawing.Point(14, 132);
+            this.label10.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(113, 35);
+            this.label10.TabIndex = 33;
+            this.label10.Text = "user 1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 30F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(897, 1055);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(btnSent2);
+            this.Controls.Add(this.txtMessage2);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.lstChatHistory);
+            this.Controls.Add(btnSend);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox2);
+            this.Font = new System.Drawing.Font("RomanD", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.ListBox lstChatHistory;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtMessage2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
---
+++ lab7/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab7
+{
+    public partial class Form1 : Form
+    {
+        private IChatRoom chatRoom;
+        private Dictionary<string, Participant> participants = new Dictionary<string, Participant>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            //initializing chat room
+            chatRoom = new ChatRoom();
+
+            //creating participants
+            participants["User1"] = new User("User1");
+            participants["User2"] = new User("User2");
+
+            //registrating participants with the chat room
+            foreach (var participant in participants.Values)
+            {
+                chatRoom.Register(participant);
+            }
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            string message = txtMessage.Text.Trim();
+            if (!string.IsNullOrEmpty(message))
+            {
+                //send message to chat room
+                participants["User1"].Send(message); //User1 is the sender for this example
+
+                //display sent message in chat history
+                lstChatHistory.Items.Add($"User1: {message}");
+
+                //clear message textbox
+                txtMessage.Clear();
+            }
+        }
+
+        private void btnSent2_Click(object sender, EventArgs e)
+        {
+            string message = txtMessage2.Text.Trim();
+            if (!string.IsNullOrEmpty(message))
+            {
+                //send message to chat room
+                participants["User2"].Send(message); //User2 is the sender for this example
+
+                //display sent message in chat history
+                lstChatHistory.Items.Add($"User2: {message}");
+
+                //clear message textbox
+                txtMessage.Clear();
+            }
+        }
+
+
+    }
+
+
+
+
+
+
+    public abstract class Participant
+    {
+        public string Name { get; }
+        public IChatRoom ChatRoom { get; set; } 
+
+        public Participant(string name)
+        {
+            Name = name;
+        }
+
+        public void Send(string message)
+        {
+            ChatRoom.SendMessage(message, this);
+        }
+
+        public abstract void ReceiveMessage(string message);
+    }
+
+
+    public class User : Participant
+    {
+        public User(string name) : base(name) { }
+
+        public override void ReceiveMessage(string message) { }
+    }
+
+    //mediator interface
+    public interface IChatRoom
+    {
+        void SendMessage(string message, Participant participant);
+        void Register(Participant participant);
+    }
+
+
+    //concrete mediator
+    public class ChatRoom : IChatRoom
+    {
+        private Dictionary<string, Participant> participants = new Dictionary<string, Participant>();
+
+        public void Register(Participant participant)
+        {
+            participants[participant.Name] = participant;
+            participant.ChatRoom = this;
+        }
+        public void SendMessage(string message, Participant sender)
+        {
+            foreach (var participant in participants.Values)
+            {
+                if (participant != sender) //exclude the sender
+                    participant.ReceiveMessage(message);
+            }
+        }
+
+    }
+
+}
+
